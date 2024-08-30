a99bb2bd513b67b8e1624c935514991582a06efa
dummy <dummy@mail.com>
2024-08-30 20:31:13 -0300

    MultiRotatingLogger: First successful and failing instatiations test cases were implemented

--- m3t_MultiRotatingLogger.py
+++ m3t_MultiRotatingLogger.py
@@ -8,4 +8,10 @@ class UnavailableNameException(Exception):
 
 
 class MultiRotatingLogger:
-    pass
+    def __init__(self, configs):
+        self.__configs = configs
+        self.__validate_configs()
+
+    def __validate_configs(self):
+        if len(self.__configs.values()) == 0:
+            raise InvalidConfigsException('Configs dict is empty.')
--- tests/m3t_MultiRotatingLogger_test.py
+++ tests/m3t_MultiRotatingLogger_test.py
@@ -10,10 +10,19 @@ def new_instance():
 @pytest.mark.parametrize('configs,expected_exception', [
     ({}, InvalidConfigsException()),
 ])
-def test_create(configs, expected_exception):
+def test_instantiation_bad_input(configs, expected_exception):
+    """Test that the expected exceptions are thrown."""
     try:
         MultiRotatingLogger(configs)
     except (InvalidConfigsException) as current_exception:
         assert isinstance(current_exception, type(expected_exception))
     else:
         pytest.fail(f'Exception \"{type(expected_exception).__name__}\" was expected, but found none')
+
+
+@pytest.mark.parametrize('configs', [
+    ({'name': 'name_test_000'}),
+])
+def test_instantiation_success(configs):
+    """Test successful instantiation."""
+    MultiRotatingLogger(configs)
