5b75109b9a47a39dc93579ac44c70dd33427169e
dummy <dummy@mail.com>
2024-09-01 18:20:27 -0300

    Utils: Validation Type method was documented and further test cases were validated

--- m3t_Utils.py
+++ m3t_Utils.py
@@ -3,8 +3,25 @@ class InvalidTypeException(Exception):
     pass
 
 
-class Validation:
+class InvalidLengthException(Exception):
+    pass
 
-    def type(self, object_to_validate, expected_type: type):
-        if not isinstance(object_to_validate, expected_type):
+
+class Validation:
+    """
+    A set of generic validation methods.
+    All exposed methods return nothing. If something is wrong then an exception is thrown.
+    """
+
+    def type(self, object_to_validate, expected_type: type, reversed_validation: bool = False):
+        """
+        Checks the object type against the type provided.
+        :param object_to_validate: An object of any type, to be validated.
+        :param expected_type: The type that is expected.
+        :param reversed_validation: If this is a truthy value, the validation is reversed.
+        """
+        validation_result = isinstance(object_to_validate, expected_type)
+        if reversed_validation:
+            validation_result = not validation_result
+        if not validation_result:
             raise InvalidTypeException(f'Object must be of type: {expected_type.__name__}.')
--- tests/m3t_Utils_test.py
+++ tests/m3t_Utils_test.py
@@ -7,28 +7,34 @@ def new_instance():
     return Validation()
 
 
-@pytest.mark.parametrize('object_to_validate,expected_type,expected_exception', [
-    ({}, list, InvalidTypeException()),
-    ({}, int, InvalidTypeException()),
-    ({}, str, InvalidTypeException()),
-    ('', dict, InvalidTypeException()),
-    ('', list, InvalidTypeException()),
-    (0, float, InvalidTypeException()),
-    (0.0, int, InvalidTypeException()),
+@pytest.mark.parametrize('object_to_validate,expected_type,reversed_validation,expected_exception', [
+    ({}, list, None, InvalidTypeException()),
+    ({}, int, None, InvalidTypeException()),
+    ({}, str, None, InvalidTypeException()),
+    ('', dict, None, InvalidTypeException()),
+    ('', list, None, InvalidTypeException()),
+    (0, float, None, InvalidTypeException()),
+    (0.0, int, None, InvalidTypeException()),
+    (0, int, True, InvalidTypeException()),
+    (0.0, float, 1, InvalidTypeException()),
 ])
-def test_type_failure(new_instance, object_to_validate, expected_type, expected_exception):
+def test_type_failure(new_instance, object_to_validate, expected_type, reversed_validation, expected_exception):
     try:
-        new_instance.type(object_to_validate, expected_type)
+        new_instance.type(object_to_validate, expected_type, reversed_validation)
     except InvalidTypeException as current_exception:
         assert isinstance(current_exception, type(expected_exception))
     else:
         pytest.fail(f'Exception \"{type(expected_exception).__name__}\" was expected, but found none')
 
 
-@pytest.mark.parametrize('object_to_validate,expected_type', [
-    ({}, dict),
-    (0, int),
-    (0.0, float),
+@pytest.mark.parametrize('object_to_validate,expected_type,reversed_validation', [
+    ({}, dict, None),
+    (0, int, None),
+    (0.0, float, None),
+    (0.0, float, False),
+    (0.0, float, 0),
+    (0, float, 1),
+    ('aaa', dict, True),
 ])
-def test_type_success(new_instance, object_to_validate, expected_type):
-    new_instance.type(object_to_validate, expected_type)
+def test_type_success(new_instance, object_to_validate, reversed_validation, expected_type):
+    new_instance.type(object_to_validate, expected_type, reversed_validation)
