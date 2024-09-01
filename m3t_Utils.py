dd252b6e51a965899a7f375d8818c821753a6dac
dummy <dummy@mail.com>
2024-09-01 18:45:50 -0300

    Utils Validation: Length validation was implemented

--- m3t_Utils.py
+++ m3t_Utils.py
@@ -7,6 +7,10 @@ class InvalidLengthException(Exception):
     pass
 
 
+class InvalidRangeException(Exception):
+    pass
+
+
 class Validation:
     """
     A set of generic validation methods.
@@ -25,3 +29,33 @@ class Validation:
             validation_result = not validation_result
         if not validation_result:
             raise InvalidTypeException(f'Object must be of type: {expected_type.__name__}.')
+
+    def length(self, object_to_validate, expected_range: tuple):
+        """
+        Checks the length of the object against the expected range.
+        :param object_to_validate: An object of any type, to be validated.
+        :param expected_range: A tuple in the form [from, to); None means no limit in that direction.
+        """
+        self.type(expected_range, tuple)
+        if len(expected_range) != 2:
+            raise InvalidLengthException(f'The expected_range parameter should be a tuple containing [from, to); None means no limit in that direction.')
+
+        object_length = len(object_to_validate)
+        min_length = expected_range[0]
+        max_length = expected_range[1]
+
+        if min_length is not None and max_length is not None:
+            self.type(min_length, int)
+            self.type(max_length, int)
+            if min_length >= max_length:
+                raise InvalidRangeException(f'The minimum value provided ({min_length}) must be lower than the maximum value provided ({max_length}).')
+
+        if min_length is not None:
+            self.type(min_length, int)
+            if object_length < min_length:
+                raise InvalidLengthException(f'Object length ({object_length}) is below the minimum expected ({min_length}).')
+
+        if max_length is not None:
+            self.type(max_length, int)
+            if object_length >= max_length:
+                raise InvalidLengthException(f'Object length ({object_length}) is above the maximum expected ({max_length}).')
--- tests/m3t_Utils_test.py
+++ tests/m3t_Utils_test.py
@@ -1,5 +1,5 @@
 import pytest
-from m3t_Utils import Validation, InvalidTypeException
+from m3t_Utils import Validation, InvalidTypeException, InvalidLengthException, InvalidRangeException
 
 
 @pytest.fixture
@@ -38,3 +38,44 @@ def test_type_failure(new_instance, object_to_validate, expected_type, reversed_
 ])
 def test_type_success(new_instance, object_to_validate, reversed_validation, expected_type):
     new_instance.type(object_to_validate, expected_type, reversed_validation)
+
+
+@pytest.mark.parametrize('object_to_validate,expected_range,expected_exception', [
+    ([], 2, InvalidTypeException()),
+    ([], {}, InvalidTypeException()),
+    ([], 'asdf', InvalidTypeException()),
+    ([], [], InvalidTypeException()),
+    ([], (), InvalidLengthException()),
+    ([1, 2, 3], (1, 3), InvalidLengthException()),
+    ([1, 2, 3], (None, 3), InvalidLengthException()),
+    ([1, 2, 3], (0, 2), InvalidLengthException()),
+    ([1, 2, 3], (4, None), InvalidLengthException()),
+    ({}, (4, None), InvalidLengthException()),
+    (1, (4, None), TypeError()),
+    (True, (4, None), TypeError()),
+    ('asdf', ('', None), InvalidTypeException()),
+    ('asdf', (3, {}), InvalidTypeException()),
+    ('asdf', (3, 3), InvalidRangeException()),
+    ('asdf', (4, 3), InvalidRangeException()),
+])
+def test_length_failure(new_instance, object_to_validate, expected_range, expected_exception):
+    try:
+        new_instance.length(object_to_validate, expected_range)
+    except (InvalidTypeException, InvalidLengthException, TypeError, InvalidRangeException) as current_exception:
+        assert isinstance(current_exception, type(expected_exception))
+    else:
+        pytest.fail(f'Exception \"{type(expected_exception).__name__}\" was expected, but found none')
+
+
+@pytest.mark.parametrize('object_to_validate,expected_range', [
+    ({}, (None, None)),
+    ('asdf', (None, None)),
+    ([1, 2, 3], (2, 5)),
+    ([1, 2, 3], (3, 5)),
+    ([1, 2, 3], (None, 4)),
+    ([1, 2, 3], (3, 4)),
+    ('asdf', (3, None)),
+    ('asdf', (None, 9)),
+])
+def test_length_success(new_instance, object_to_validate, expected_range):
+    new_instance.length(object_to_validate, expected_range)
