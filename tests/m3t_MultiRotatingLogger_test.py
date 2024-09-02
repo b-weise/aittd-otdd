84d72d2d83604419dc026e8cfc4f59c1a6c54dae
dummy <dummy@mail.com>
2024-09-02 18:26:34 -0300

    MultiRotatingLogger: Code was refactored based on the changes in Utils

--- tests/m3t_MultiRotatingLogger_test.py
+++ tests/m3t_MultiRotatingLogger_test.py
@@ -1,24 +1,24 @@
 import pytest
 
 from m3t_MultiRotatingLogger import MultiRotatingLogger
-from m3t_Utils import InvalidTypeException, InvalidLengthException, KeyExistenceException
+from m3t_Utils import ExpectedTypeException, ExpectedKeyException, MinimumLengthException
 
 
 @pytest.mark.parametrize('configs,expected_exception', [
-    ('aaa', InvalidTypeException()),
-    ({'name': 'asdf'}, InvalidTypeException()),
-    (['aaa'], InvalidTypeException()),
-    ([{}], KeyExistenceException()),
-    ([{}, {}], KeyExistenceException()),
-    ([{'name': 1234}], InvalidTypeException()),
-    ([{}, {'name': 'asdf'}], KeyExistenceException()),
-    ([{'name': ''}, {'name': 'asdf'}], InvalidLengthException()),
-    ([{'name': 'asdf'}, {'name': ['a', 's', 'd']}], InvalidTypeException()),
+    ('aaa', ExpectedTypeException()),
+    ({'name': 'asdf'}, ExpectedTypeException()),
+    (['aaa'], ExpectedTypeException()),
+    ([{}], ExpectedKeyException()),
+    ([{}, {}], ExpectedKeyException()),
+    ([{'name': 1234}], ExpectedTypeException()),
+    ([{}, {'name': 'asdf'}], ExpectedKeyException()),
+    ([{'name': ''}, {'name': 'asdf'}], MinimumLengthException()),
+    ([{'name': 'asdf'}, {'name': ['a', 's', 'd']}], ExpectedTypeException()),
 ])
 def test_instantiation_failure(configs, expected_exception):
     try:
         MultiRotatingLogger(configs)
-    except (InvalidTypeException, InvalidLengthException, KeyExistenceException) as current_exception:
+    except (ExpectedTypeException, ExpectedKeyException, MinimumLengthException) as current_exception:
         assert isinstance(current_exception, type(expected_exception))
     else:
         pytest.fail(f'Exception \"{type(expected_exception).__name__}\" was expected, but found none')
