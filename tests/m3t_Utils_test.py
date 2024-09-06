3e84dcdc23f823a0ba10a0940bc8d888c4cfefaf
dummy <dummy@mail.com>
2024-09-05 21:18:36 -0300

    Utils Validation: BaseTestCase is imported from an external file

--- tests/m3t_Utils_test.py
+++ tests/m3t_Utils_test.py
@@ -1,36 +1,14 @@
 import dataclasses
 import sys
 from dataclasses import dataclass
-from typing import Any, Optional, Type
+from typing import Any
 
 import pytest
 
+from m3t_BaseTestCase import BaseTestCase
 from m3t_Utils import (Validation, ForbiddenTypeException, MandatoryTypeException, MinimumLengthException,
                        MaximumLengthException, InvalidRangeLengthException, InvalidRangeValuesException,
                        ForbiddenKeyException, MandatoryKeyException)
-
-
-@dataclass
-class BaseTestCase:
-    """Base class for test cases."""
-    id: Optional[str] = dataclasses.field(default=None)
-    __id: str = dataclasses.field(init=False, repr=False)
-    expected_exception: Optional[Type[Exception]] = None
-
-    @property
-    def id(self) -> str | None:
-        """
-        Getter of the "id" property.
-        :return: A string formatted in such a way it's easily recognized in pytest logs.
-        """
-        if isinstance(self.__id, str):
-            return f'--- {self.__id} ---'.upper()
-        else:
-            return None
-
-    @id.setter
-    def id(self, provided_id: str):
-        self.__id = provided_id
 
 
 @pytest.fixture
