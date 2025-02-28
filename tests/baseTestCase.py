5f149a51dbcdf5a3d55183e87e53e1d4736eb5bb
Brian Weisemann <brianw7693@gmail.com>
2025-02-28 10:37:48 -0300

    BaseTestCase: Documentation was improved, 'repr' parameter was removed as it was unnecessary

--- tests/baseTestCase.py
+++ tests/baseTestCase.py
@@ -4,16 +4,20 @@ from typing import Optional, Type
 
 @dataclasses.dataclass
 class BaseTestCase:
-    """Base class for test cases."""
+    """
+    Base class for test cases.
+    """
+
+    __id: Optional[str] = dataclasses.field(default=None, repr=False)
     id: Optional[str] = dataclasses.field(default=None)
-    __id: str = dataclasses.field(init=False, repr=False)
     expected_exception: Optional[Type[Exception]] = None
 
     @property
     def id(self) -> str | None:
         """
         Getter of the "id" property.
-        :return: A string formatted in such a way it's easily recognized in pytest logs.
+        :return: If "id" is set, returns a string formatted for easy recognition in pytest logs;
+        otherwise, returns None to suppress pytest's default printing.
         """
         if isinstance(self.__id, str):
             return f'--- {self.__id} ---'.upper()
