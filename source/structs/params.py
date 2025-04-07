bba8fe415937aa67566b98961ab6ed6f405b48a6
Brian Weisemann <brianw7693@gmail.com>
2025-04-07 20:15:21 -0300

    feat(params): add initial file

---
+++ source/structs/params.py
@@ -0,0 +1,59 @@
+from collections.abc import Sequence, Callable
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Optional
+
+from pandas import DatetimeIndex
+
+
+@dataclass
+class LayerParams:
+    Units: int
+    KernelInitializer: Optional[Callable] = None
+    KernelRegularizer: Optional[Callable] = None
+    Activation: Optional[Callable] = None
+
+
+@dataclass
+class TrainingParams:
+    Hash: str
+    ColumnToPredict: str
+    WindowWidth: int
+    SetTrainingFlag: bool
+    UseResidualWrapper: bool
+    PrependBatchNormLayer: bool
+    FitMaxEpochs: int
+    FitPatience: int
+    CompileLossFunction: Callable
+    CompileOptimizer: Callable
+    LayerStack: dict[int, LayerParams]
+    DatasetPath: Path
+    DatasetTimeFilter: DatetimeIndex
+    DatasetShuffle: bool
+    DatasetBatchSize: int
+
+
+@dataclass
+class LayerParamsCombinations:
+    Units: Sequence[int]
+    KernelInitializer: Optional[Sequence[Optional[Callable]]] = None
+    KernelRegularizer: Optional[Sequence[Optional[Callable]]] = None
+    Activation: Optional[Sequence[Optional[Callable]]] = None
+
+
+@dataclass
+class TrainingParamsCombinations:
+    ColumnToPredict: Sequence[str]
+    WindowWidth: Sequence[int]
+    SetTrainingFlag: Sequence[bool]
+    UseResidualWrapper: Sequence[bool]
+    PrependBatchNormLayer: Sequence[bool]
+    FitMaxEpochs: Sequence[int]
+    FitPatience: Sequence[int]
+    CompileLossFunction: Sequence[Callable]
+    CompileOptimizer: Sequence[Callable]
+    LayerStack: Sequence[dict[int, LayerParamsCombinations]]
+    DatasetPath: Sequence[Path]
+    DatasetTimeFilter: Sequence[DatetimeIndex]
+    DatasetShuffle: Sequence[bool]
+    DatasetBatchSize: Sequence[int]
