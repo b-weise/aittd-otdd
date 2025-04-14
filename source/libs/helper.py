6c5f3c9bb905cf67f3f02d9a4a222fdbeedba8c9
Brian Weisemann <brianw7693@gmail.com>
2025-04-13 21:16:45 -0300

    feat(Helper): implement 'get_module_callable' method
    Reformat 'test_get_fully_qualified_name_success'.

--- source/libs/helper.py
+++ source/libs/helper.py
@@ -1,3 +1,4 @@
+import sys
 from collections.abc import Callable
 
 
@@ -14,3 +15,19 @@ class Helper:
         :return: A string representing the fully qualified name.
         """
         return f'{obj.__module__}.{obj.__qualname__}'
+
+    @staticmethod
+    def get_module_callable(fully_qualified_name: str) -> Callable | None:
+        """
+        Resolves an object by its fully qualified name.
+        :param fully_qualified_name: The fully qualified name of the target object.
+        :return: The resolved object if found; otherwise, None.
+        """
+        subpaths = fully_qualified_name.split('.')
+        module_name = subpaths.pop(0)
+        callable_object = None
+        if module_name in sys.modules:
+            module_obj = sys.modules[module_name]
+            for subpath in subpaths:
+                callable_object = getattr(callable_object or module_obj, subpath, None)
+        return callable_object
--- tests/libs/helper_test.py
+++ tests/libs/helper_test.py
@@ -18,19 +18,42 @@ GFQNameMethTC = GetFullyQualifiedNameMethodTestCase
 
 
 @pytest.mark.parametrize('test_case', [pytest.param(test_case, id=test_case.id) for test_case in [
-    GFQNameMethTC(input_value=divmod,
-                  expected_output='builtins.divmod'),
-    GFQNameMethTC(input_value=min,
-                  expected_output='builtins.min'),
-    GFQNameMethTC(input_value=BaseTestCase,
-                  expected_output='source.libs.baseTestCase.BaseTestCase'),
-    GFQNameMethTC(input_value=Helper,
-                  expected_output='source.libs.helper.Helper'),
-    GFQNameMethTC(input_value=pandas.DataFrame,
-                  expected_output='pandas.core.frame.DataFrame'),
-    GFQNameMethTC(input_value=pandas.Series,
-                  expected_output='pandas.core.series.Series'),
+    GFQNameMethTC(input_value=divmod, expected_output='builtins.divmod'),
+    GFQNameMethTC(input_value=min, expected_output='builtins.min'),
+    GFQNameMethTC(input_value=BaseTestCase, expected_output='source.libs.baseTestCase.BaseTestCase'),
+    GFQNameMethTC(input_value=Helper, expected_output='source.libs.helper.Helper'),
+    GFQNameMethTC(input_value=pandas.DataFrame, expected_output='pandas.core.frame.DataFrame'),
+    GFQNameMethTC(input_value=pandas.Series, expected_output='pandas.core.series.Series'),
 ]])
 def test_get_fully_qualified_name_success(test_case: GetFullyQualifiedNameMethodTestCase):
     computed_output = Helper.get_fully_qualified_name(test_case.input_value)
+    assert computed_output == test_case.expected_output
+
+
+@dataclass
+class GetModuleCallableMethodTestCase(BaseTestCase):
+    input_value: str
+    expected_output: Callable | None
+
+
+GMCallableMethTC = GetModuleCallableMethodTestCase
+
+
+@pytest.mark.parametrize('test_case', [pytest.param(test_case, id=test_case.id) for test_case in [
+    GMCallableMethTC(id='found callables',
+                     input_value='builtins.divmod', expected_output=divmod),
+    GMCallableMethTC(input_value='builtins.min', expected_output=min),
+    GMCallableMethTC(input_value='source.libs.baseTestCase.BaseTestCase', expected_output=BaseTestCase),
+    GMCallableMethTC(input_value='source.libs.helper.Helper', expected_output=Helper),
+    GMCallableMethTC(input_value='pandas.core.frame.DataFrame', expected_output=pandas.DataFrame),
+    GMCallableMethTC(input_value='pandas.core.series.Series', expected_output=pandas.Series),
+    GMCallableMethTC(id='installed module as input',
+                     input_value='pandas', expected_output=None),
+    GMCallableMethTC(input_value='pytest', expected_output=None),
+    GMCallableMethTC(id='non-installed module as input',
+                     input_value='scrapy', expected_output=None),
+    GMCallableMethTC(input_value='aaaaaa', expected_output=None),
+]])
+def test_get_module_callable_success(test_case: GetModuleCallableMethodTestCase):
+    computed_output = Helper.get_module_callable(test_case.input_value)
     assert computed_output == test_case.expected_output
