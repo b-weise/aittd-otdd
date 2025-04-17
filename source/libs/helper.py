fbd4e2d852472fd3cdbc80433cfa3b1c96cadbf3
Brian Weisemann <brianw7693@gmail.com>
2025-04-17 14:53:35 -0300

    refactor(helper): re-implement 'stringify_objects' for clarity

--- source/libs/helper.py
+++ source/libs/helper.py
@@ -43,35 +43,34 @@ class Helper:
         :return: A stringified version of the input if applicable; otherwise, the original input.
         """
 
-        def is_container(value: Any) -> bool:
-            """
-            Determines whether the given object is considered a container.
-            In this context, a 'container' refers to dictionaries and Sequences, excluding strings.
-            :param value: The object to evaluate.
-            :return: True if the object is a container; False otherwise.
-            """
-            return isinstance(value, Sequence | dict) and type(value) is not str
-
         def stringify(value: Any) -> Any:
             """
             Converts the given object to its string representation.
-            If the object is considered a container, it is processed by stringify_objects.
             :param value: The object to be stringified.
             :return: A stringified version of the object if applicable; otherwise, the original object.
             """
-            if is_container(value):
-                return Helper.stringify_objects(value)
-            elif isinstance(value, Path | datetime):
+            if isinstance(value, Path | datetime):
                 return str(value)
             elif isinstance(value, Callable):
                 return Helper.get_fully_qualified_name(value)
             else:
                 return value
 
-        if type(raw_input) is dict:
-            return {key: (stringify(value))
-                    for key, value in zip(raw_input.keys(), raw_input.values())}
-        elif is_container(raw_input):
-            return list(map(lambda item: (stringify(item)), raw_input))
-        else:
-            return stringify(raw_input)
+        def walk_objects(input_obj: Any, callback: Callable) -> Any:
+            """
+            Recursively traverses the given object in a depth-first fashion,
+            invoking the callback on each plain value encountered.
+            :param input_obj: The object to traverse.
+            :param callback: A callback used to process plain values.
+            :return: Either, a dictionary or list with every value processed by the callback,
+            or the output of the callback itself.
+            """
+            if isinstance(input_obj, dict):
+                return {key: (walk_objects(value, callback))
+                        for key, value in zip(input_obj.keys(), input_obj.values())}
+            elif isinstance(input_obj, Sequence) and not isinstance(input_obj, str):
+                return list(map(lambda item: (walk_objects(item, callback)), input_obj))
+            else:
+                return callback(input_obj)
+
+        return walk_objects(raw_input, stringify)
