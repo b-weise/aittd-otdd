658a48ac1bd8eb2b51b3a5546b5661c016befe03
Brian Weisemann <brianw7693@gmail.com>
2025-04-17 14:25:42 -0300

    fix: revert
    Reverted commits:
    	d75531dde1214a887c7a73f7d44ae82390c3c5e3
    		feat(helper): remove 'stringify_objects' method
    		It was originally intended to help generate hashes from objects.
    		deepdiff.DeepHash will be used instead.
    	a9b0320ea190f7ef6974f0521e44208bf480b7d4
    		build(requirements): add deepdiff
    
    DeepHash includes memory-specific binary positions for some callables (e.g.,
    <function RMSprop.get_config at 0x7fd6e7260ea0>), causing hash values to change
    across executions. Since this behavior cannot be avoided, I'm returning to a
    custom solution.

--- source/libs/helper.py
+++ source/libs/helper.py
@@ -1,5 +1,8 @@
 import sys
-from collections.abc import Callable
+from collections.abc import Callable, Sequence
+from datetime import datetime
+from pathlib import Path
+from typing import Any
 
 
 class Helper:
@@ -31,3 +34,44 @@ class Helper:
             for subpath in subpaths:
                 callable_object = getattr(callable_object or module_obj, subpath, None)
         return callable_object
+
+    @staticmethod
+    def stringify_objects(raw_input: Any) -> Any:
+        """
+        Stringifies the given object or a container of objects.
+        :param raw_input: The object to be stringified, or a container that will be traversed recursively.
+        :return: A stringified version of the input if applicable; otherwise, the original input.
+        """
+
+        def is_container(value: Any) -> bool:
+            """
+            Determines whether the given object is considered a container.
+            In this context, a 'container' refers to dictionaries and Sequences, excluding strings.
+            :param value: The object to evaluate.
+            :return: True if the object is a container; False otherwise.
+            """
+            return isinstance(value, Sequence | dict) and type(value) is not str
+
+        def stringify(value: Any) -> Any:
+            """
+            Converts the given object to its string representation.
+            If the object is considered a container, it is processed by stringify_objects.
+            :param value: The object to be stringified.
+            :return: A stringified version of the object if applicable; otherwise, the original object.
+            """
+            if is_container(value):
+                return Helper.stringify_objects(value)
+            elif isinstance(value, Path | datetime):
+                return str(value)
+            elif isinstance(value, Callable):
+                return Helper.get_fully_qualified_name(value)
+            else:
+                return value
+
+        if type(raw_input) is dict:
+            return {key: (stringify(value))
+                    for key, value in zip(raw_input.keys(), raw_input.values())}
+        elif is_container(raw_input):
+            return list(map(lambda item: (stringify(item)), raw_input))
+        else:
+            return stringify(raw_input)
